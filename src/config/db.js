e251262996f22f9eff82e9ce54a2e7f2243cdf30
Sabuj Gain <green.sabuj.2012@gmail.com>
2025-09-28 21:38:32 +0530

    update

--- src/config/db.js
+++ src/config/db.js
@@ -16,13 +16,19 @@
 // };
 
 // export default connectDB;
+// config/db.js
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI, {
+      dbName: "pochiroot",      // ✅ Specify your database name here
+      // optional settings
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log("✅ MongoDB connected");
   } catch (error) {
     console.error("❌ MongoDB connection failed:", error);
